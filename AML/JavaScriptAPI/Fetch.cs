c13fed16fe8e2e9fd42b8de94305cc6fdaf61303
MinwooWebeng <mwsong@webeng.kaist.ac.kr>
2025-09-10 14:19:02 +0900

    .

--- AML/JavaScriptAPI/Fetch.cs
+++ AML/JavaScriptAPI/Fetch.cs
@@ -1,7 +1,6 @@
 ﻿
 #nullable enable
 #pragma warning disable IDE1006 //naming convension
-using AbyssCLI.HL;
 using Microsoft.ClearScript;
 using Microsoft.ClearScript.JavaScript;
 using Microsoft.ClearScript.V8;
@@ -53,70 +52,3 @@ public class Response
     }
     public object text() => JavaScriptExtensions.ToPromise(_native_response.Content.ReadAsStringAsync(), _origin.Engine);
 }
-
-//using Microsoft.ClearScript.JavaScript;
-
-//#nullable enable
-//#pragma warning disable IDE1006 //naming convension
-//namespace AbyssCLI.AML.JavaScriptAPI;
-//internal class WebFetchApi()
-//{
-//    // Fetch the content from a URL
-//    public async Task<Response> FetchAsync(string resource)
-//    {
-//        return await FetchAsync(resource, new RequestInit
-//        {
-//            method = "GET",
-//            body = null,
-//        });
-//    }
-//    public async Task<Response> FetchAsync(string resource, dynamic _options)
-//    {
-//        RequestInit options;
-//        options = new RequestInit
-//        {
-//            method = _options.method,
-//            body = _options.body,
-//        };
-//        return options.method switch
-//        {
-//            "GET" => new Response(await _resource_loader.TryHttpGetAsync(resource)),
-//            "POST" => new Response(await _resource_loader.TryHttpPostAsync(resource, options.body switch
-//            {
-//                string text => new StringContent(text),
-//                object o => new StringContent(o.ToString()),
-//                _ => null
-//            })),
-//            _ => throw new Exception("not supported request method"),
-//        };
-//    }
-//}
-//public class RequestInit
-//{
-//    public string method;
-//    public object body;
-//}
-//public class Response
-//{
-//#pragma warning disable IDE1006 //naming convention
-//    public Response(HttpResponseMessage native_response)
-//    {
-//        ok = native_response.IsSuccessStatusCode;
-//        status = (int)native_response.StatusCode;
-//        statusText = native_response.StatusCode.ToString();
-
-//        _native_response = native_response;
-//    }
-//    public Headers headers;
-//    public bool ok;
-//    public int status;
-//    public string statusText;
-
-//    private readonly HttpResponseMessage _native_response;
-//    public object text() => JavaScriptExtensions.ToPromise(_native_response.Content.ReadAsStringAsync());
-//#pragma warning restore IDE1006
-//}
-//public class Headers
-//{
-
-//}
